7ef45f6e2b53acf41223ba7814d5b35ee76d3cf5
saclauw <saclauw@gmail.com>
2018-01-23 14:19:32 -0500

    another commit to LabNumber2

--- src/LabNumber2.java
+++ src/LabNumber2.java
@@ -1,4 +1,39 @@
+/* Steven Clauw LabNumber2
+ * 
+ * Use conditional statements to automate the decision making process
+ * the application will prompt the user to enter an integer between 1 and 100
+ * Display the correct response based on the integer range entered
+ * Extra - end the program officially,
+ * 		 - Ask for username and refer to the user by name
+ * 		 - Add validation to gurarantee the user enters a positive integer between 1 and 100
+ * 
+ * Build Specs
+ * 	If/else statement to take different actions depending on input
+ * 		if odd, print number and "Odd"
+ * 		if even between 2 and 25, print "Even and less than 25"
+ * 	`	if even between 26 and 60, print "Even"
+ * 		if even and greater than 60, print the number entered and "Even"
+ * 		if odd and greater than 60, print the number entered "Odd and over 60"
+ * 
+ */
+
+import java.util.Scanner;
 
 public class LabNumber2 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int userNum;
+		boolean isUserNumNum;
+		
+		System.out.println("Please enter your name: ");
+		String userName = scan.nextLine();
+		System.out.println("Hello, " + userName + " enter a number between 1 and 100");
+		
+		do {
+			do {
+				
+			}
+		}
+	}
 }
